49a930c419796ad847779962e0c9ea409053b231
Al Momin <amomin@evermight.com>
2014-05-04 20:15:09 -0400

    solves 100

--- python/p91.py
+++ python/p91.py
@@ -1,13 +1,10 @@
 import math, time, sys
 sys.path.insert(0,'./lib')
 from NumberTheory import gcd
-
 ceil=math.ceil
 floor=math.floor
 
 SIZE=50
-
-# Ignore x1=0,y1=0 for now, these are easy cases
 
 def relPrime(x1,y1):
 	g = gcd(x1,y1)
@@ -31,17 +28,10 @@ for x1 in range(1,SIZE+1):
 	for y1 in range(1,SIZE+1):
 		lo = lowBound(x1,y1)
 		hi = upBound(x1,y1)
-
 		if hi>lo:
-			#print "x1,y1 is ", x1,y1," k between ", lo, hi
 			count += (hi-lo)
-		#else:
-			#print "x1,y1 no good:", x1,y1
 
 print "COunted ", count, "triangles with a right angle at an interior point"
-
 print "There are ",2*SIZE*SIZE, " triangles with a right angle at an edge point"
-
 print "There are ",SIZE*SIZE," triangles with a right angle at O"
-
 print "TOtal is ", 3*SIZE*SIZE + count
